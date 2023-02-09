5fe1cd24663f27eea04b1a86364eab89e075a0e9
Hipster16 <100425953+Hipster16@users.noreply.github.com>
2023-02-10 01:05:02 +0530

    quick fix 2

--- src/pages/Components/PasswordBlock.java
+++ src/pages/Components/PasswordBlock.java
@@ -31,164 +31,162 @@ public class PasswordBlock extends JPanel implements Colorlib {
     public MainWindow parent;
     public PasswordBlock block;
     public ResultSet row;
-    public String user,field1,field2,field3;
-    public PasswordBlock (int width,int height,MainWindow parent,ResultSet row,String user) throws SQLException{
-        this.parent=parent;
-        this.block=this;
-        this.row=row;
-        this.user=user;
-        field1=row.getString("username");
-        field2=row.getString("password");
-        field3=row.getString("Site");
+    public String user, field1, field2, field3;
+
+    public PasswordBlock(int width, int height, MainWindow parent, ResultSet row, String user) throws SQLException {
+        this.parent = parent;
+        this.block = this;
+        this.row = row;
+        this.user = user;
+        field1 = row.getString("username");
+        field2 = row.getString("password");
+        field3 = row.getString("Site");
         setBorder(new RoundedBorder(20, lightGray));
         setOpaque(false);
-        setPreferredSize(new Dimension(width,height));
-        setLayout(new GridLayout(1,4));
+        setPreferredSize(new Dimension(width, height));
+        setLayout(new GridLayout(1, 4));
 
-        JLabel userpane=new JLabel(field1);
+        JLabel userpane = new JLabel(field1);
         userpane.setForeground(Color.white);
 
-        JLabel passpane=new JLabel(row.getString("password"));
+        JLabel passpane = new JLabel(field2);
         passpane.setForeground(Color.white);
 
-        JLabel sitepane=new JLabel(field2);
+        JLabel sitepane = new JLabel(field3);
         sitepane.setForeground(Color.white);
-        
+
         JButton edit = new JButton("edit");
         edit.setForeground(Color.black);
         edit.setFont(new Font("Dialog", Font.BOLD, 15));
         edit.setBackground(Color.green);
-        edit.setPreferredSize(new Dimension(100,30));
+        edit.setPreferredSize(new Dimension(100, 30));
         edit.setBorder(BorderFactory.createRaisedBevelBorder());
         edit.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                AddEdit a=new AddEdit("Edit");
+                AddEdit a = new AddEdit("Edit");
+                a.userTxt.setText(field1);
+                a.userTxt.setFont(new Font("Dialog", Font.PLAIN, 17));
+                a.userTxt.setForeground(Color.white);
+                a.passTxt.setText(field2);
+                a.passTxt.setFont(new Font("Dialog", Font.PLAIN, 17));
+                a.passTxt.setForeground(Color.white);
+                a.siteTxt.setText(field3);
+                a.siteTxt.setFont(new Font("Dialog", Font.PLAIN, 17));
+                a.siteTxt.setForeground(Color.white);
                 a.submit.addActionListener(new ActionListener() {
 
                     @Override
                     public void actionPerformed(ActionEvent e) {
-                        String users=a.userTxt.getText();
-                        String site=a.siteTxt.getText();
-                        String pass=a.passTxt.getText();
-                        try{
-                            MainConnector obj=new MainConnector(parent.user);
-                            obj.rs=obj.statement.executeQuery(obj.selectAllFromtable());
-                            boolean flag=true;
-                            while(obj.rs.next()){
-                                if(users.equals(obj.rs.getString("username")) && site.equals(obj.rs.getString("Site")) ){
-                                    flag=false;
-                                    a.invalidLabel.setText("This account already exist");
-                                    break;
-                                }
-                                else if(((users.equals("Enter the username") || site.equalsIgnoreCase("Enter the password" )||
-                                site.equals("Enter the siteName")))){
-                                    a.invalidLabel.setText("Fill the form properly");
-                                    flag=false;
-                                    break;
-                                }
-                                else if(((users.equals("") || site.equalsIgnoreCase("" )||
-                                site.equals("")))){
-                                    a.invalidLabel.setText("Fill the form properly");
-                                    flag=false;
-                                    break;
-                                }
+                        String users = a.userTxt.getText();
+                        String site = a.siteTxt.getText();
+                        String pass = a.passTxt.getText();
+                        try {
+                            MainConnector obj = new MainConnector(parent.user);
+                            boolean flag = true;
+                            if (((users.equals("Enter the username") || site.equalsIgnoreCase("Enter the password") ||
+                                    site.equals("Enter the siteName")))) {
+                                a.invalidLabel.setText("Fill the form properly");
+                                flag = false;
+                            } else if (((users.equals("") || site.equalsIgnoreCase("") ||
+                                    site.equals("")))) {
+                                a.invalidLabel.setText("Fill the form properly");
+                                flag = false;
                             }
-                            if(flag){
-                                String userpaneTxt=userpane.getText();
-                                String sitepaneTxt=sitepane.getText();
-                                obj.statement.executeUpdate(obj.updateTable(users,pass,site,userpaneTxt,sitepaneTxt));
-                                field1=users;
-                                field2=site;
-                                field3=pass;
+                            if (flag) {
+                                String userpaneTxt = userpane.getText();
+                                String sitepaneTxt = sitepane.getText();
+                                obj.statement
+                                        .executeUpdate(obj.updateTable(users, pass, site, userpaneTxt, sitepaneTxt));
+                                field1 = users;
+                                field2 = pass;
+                                field3 = site;
                                 userpane.setText(users);
-                                passpane.setText(site);
-                                sitepane.setText(pass);
+                                passpane.setText(pass);
+                                sitepane.setText(site);
                                 parent.frame.setVisible(false);
                                 parent.frame.setVisible(true);
                                 a.frame.dispose();
-                            }
-                            else{
-					            a.userTxt.setText("");
-					            a.passTxt.setText("");
+                            } else {
+                                a.userTxt.setText("");
+                                a.passTxt.setText("");
                                 a.siteTxt.setText("");
-					            a.userTxt.setBorder(new LineBorder(new Color(red),2));
-					            a.passTxt.setBorder(new LineBorder(new Color(red),2));
-                                a.siteTxt.setBorder(new LineBorder(new Color(red),2));
+                                a.userTxt.setBorder(new LineBorder(new Color(red), 2));
+                                a.passTxt.setBorder(new LineBorder(new Color(red), 2));
+                                a.siteTxt.setBorder(new LineBorder(new Color(red), 2));
                                 a.frame.addMouseMotionListener(new MouseMotionListener() {
 
                                     @Override
                                     public void mouseDragged(MouseEvent e) {
                                         // TODO Auto-generated method stub
-                                        
+
                                     }
 
                                     @Override
                                     public void mouseMoved(MouseEvent e) {
-                                        if(a.invalidLabel.getText().equals("This account already exist")||a.invalidLabel.getText().equals("Fill the form properly")){
+                                        if (a.invalidLabel.getText().equals("This account already exist")
+                                                || a.invalidLabel.getText().equals("Fill the form properly")) {
                                             a.invalidLabel.setText("");
                                             a.userTxt.requestFocus();
                                             a.passTxt.requestFocus();
                                             a.siteTxt.requestFocus();
                                             a.frame.requestFocus();
                                         }
-                                        
+
                                     }
-                                    
+
                                 });
                             }
                             obj.close();
-                        }
-                        catch(Exception err){
+                        } catch (Exception err) {
                             System.out.println(err);
                         }
-                        
+
                     }
 
-                    
                 });
             }
-            
+
         });
 
         delete = new JButton("X");
         delete.setBackground(new Color(red));
         delete.setForeground(Color.white);
-        delete.setPreferredSize(new Dimension(30,30));
+        delete.setPreferredSize(new Dimension(30, 30));
         delete.setBorder(BorderFactory.createRaisedBevelBorder());
         delete.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                int Bounds[]={60, 20, 400, 100};
-                Popups f=new Popups("Are You You want to delete this password","Delete","Go Back",Bounds);
+                int Bounds[] = { 60, 20, 400, 100 };
+                Popups f = new Popups("Are You You want to delete this password", "Delete", "Go Back", Bounds);
                 f.b.addActionListener(new ActionListener() {
 
                     @Override
                     public void actionPerformed(ActionEvent e) {
-                        ((JPanel)parent.centerbar.getViewport().getView()).remove(block);
-                        try{
-                            MainConnector con=new MainConnector(user);
-                            con.statement.executeUpdate(con.deleteFromTable(field1,field2,field3));
-                            con.rs=con.statement.executeQuery(con.getCount());
-                            int c=con.rs.getInt("count(*)");
-                            if(c<5){
-                                ((JPanel)parent.centerbar.getViewport().getView()).setLayout(new GridLayout(5,1,0,10));
-                            }
-                            else{
-                                ((JPanel)parent.centerbar.getViewport().getView()).setLayout(new GridLayout(c,1,0,10));
+                        ((JPanel) parent.centerbar.getViewport().getView()).remove(block);
+                        try {
+                            MainConnector con = new MainConnector(user);
+                            con.statement.executeUpdate(con.deleteFromTable(field1, field2, field3));
+                            con.rs = con.statement.executeQuery(con.getCount());
+                            int c = con.rs.getInt("count(*)");
+                            if (c < 5) {
+                                ((JPanel) parent.centerbar.getViewport().getView())
+                                        .setLayout(new GridLayout(5, 1, 0, 10));
+                            } else {
+                                ((JPanel) parent.centerbar.getViewport().getView())
+                                        .setLayout(new GridLayout(c, 1, 0, 10));
                             }
                             con.close();
-                        }
-                        catch(Exception erors){
+                        } catch (Exception erors) {
                             System.out.println(erors);
                         }
                         parent.frame.setVisible(false);
                         parent.frame.setVisible(true);
                         f.f.dispose();
                     }
-                    
+
                 });
 
                 f.b2.addActionListener(new ActionListener() {
@@ -196,15 +194,15 @@ public class PasswordBlock extends JPanel implements Colorlib {
                     @Override
                     public void actionPerformed(ActionEvent e) {
                         f.f.dispose();
-                        
+
                     }
-                    
+
                 });
             }
-            
+
         });
 
-        JPanel buttonpane=new JPanel();
+        JPanel buttonpane = new JPanel();
         buttonpane.setLayout(new FlowLayout());
         buttonpane.add(edit);
         buttonpane.add(Box.createHorizontalStrut(20));
